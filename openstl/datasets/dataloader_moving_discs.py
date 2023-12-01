fb73f6bf8c55237f3a2df92c7f8c602b9eca395f
mphamvan <mathieu.pham-van-cang@pasteur.fr>
2023-12-01 09:27:52 +0100

    generated test

--- openstl/datasets/dataloader_moving_discs.py
+++ openstl/datasets/dataloader_moving_discs.py
@@ -45,8 +45,8 @@ class MovingDISCS(Dataset):
 
         self.dataset = None
         self.is_train = is_train
-        if not is_train:
-            self.dataset = load_fixed_set(root, 'test')
+        # if not is_train:
+        #     self.dataset = load_fixed_set(root, 'test')
         self.length = int(1e4) if self.dataset is None else self.dataset.shape[1]
 
         self.n_frames_input = n_frames_input
