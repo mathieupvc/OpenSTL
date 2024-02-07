3d0cac757117b6f085cb2056558994722fcb554b
mphamvan <mathieu.pham-van-cang@pasteur.fr>
2024-02-07 17:27:28 +0100

    davis dataset fix

--- openstl/datasets/dataloader.py
+++ openstl/datasets/dataloader.py
@@ -23,7 +23,7 @@ def load_data(dataname, batch_size, val_batch_size, num_workers, data_root, dist
     elif 'kth' in dataname:  # 'kth', 'kth20', 'kth40'
         from .dataloader_kth import load_data
         return load_data(batch_size, val_batch_size, data_root, num_workers, **cfg_dataloader)
-    elif 'dataname' == 'davis':
+    elif dataname == 'davis':
         from .dataloader_davis import load_data
         return load_data(batch_size, val_batch_size, data_root, num_workers, **cfg_dataloader)
     elif dataname in ['mmnist', 'mfmnist', 'mmnist_cifar']:  # 'mmnist', 'mfmnist', 'mmnist_cifar'
